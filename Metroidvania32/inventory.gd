51892260a65f863b172cd16acbef3bb19210a6c2
wjriordan3 <70760257+wjriordan3@users.noreply.github.com>
2026-05-18 00:48:18 -0400

    Remove nested inventory class and tweak debug output

--- Metroidvania32/inventory.gd
+++ Metroidvania32/inventory.gd
@@ -1,43 +1,43 @@
 extends Node
 
-class Inventory:
-	func _init() -> void :
-		# 5x5 grid, 20 backpack slots, 5 equipped
-		# leftarm=[4][0], leftleg=[4][1], core=[4][2], rightleg=[4][3], rightarm=[4][4]
-		# equipment mirrored ordering left to right to make GUI placement more logical
-		self.contents = \
-		[
-			[null, null, null, null, null],
-			[null, null, null, null, null],
-			[null, null, null, null, null],
-			[null, null, null, null, null],
-			[null, null, null, null, null]
-		]
-	
-	#Called when the player walks over an item in the world
-	#Inputs
-	#item : Object - The target item
-	func pickup( item : Object ) -> void :
-		for row in self.contents:
-			for col in self.contents[row] :
-				if not self.contents[row][col] :
-					print( "item added to inventory at {row},{col}"  )
-					self.contents[row][col] = item
-					return
-		print( "no space for item!" )
-	
+# 5x5 grid, 20 backpack slots, 5 equipped
+# leftarm=[4][0], leftleg=[4][1], core=[4][2], rightleg=[4][3], rightarm=[4][4]
+# equipment mirrored ordering left to right to make GUI placement more logical
+func _init() -> void :
+	self.contents = \
+	[
+		[null, null, null, null, null],
+		[null, null, null, null, null],
+		[null, null, null, null, null],
+		[null, null, null, null, null],
+		[null, null, null, null, null]
+	]
+
+#Called when the player walks over an item in the world
+#Inputs
+#item : Object - The target item
+func pickup( item : Object ) -> void :
+	for row in self.contents:
+		for col in self.contents[row] :
+			if not self.contents[row][col] :
+				print( "item added to inventory at {row},{col}"  )
+				self.contents[row][col] = item
+				return
+	print( "no space for item!" )
+
 	#Called when the player selects "drop" with an inventory cell highlighted
 	#Inputs
 	#x : int - The x position of the highlighted cell
 	#y : int - The y position of the highlighted cell
 	
-	#Returns 
-	#item : Object - The selected item to be passed accordingly (eg. placed into the world or sent to workshop table)
-	func drop( x : int, y : int ):
-		var item : Object = self.contents[x][y]
-		self.contents[x][y] = null
-		return item
-		
+#Returns 
+#item : Object - The selected item to be passed accordingly (eg. placed into the world or sent to workshop table)
+func drop( x : int, y : int ):
+	var item : Object = self.contents[x][y]
+	self.contents[x][y] = null
+	print( item.to_string())
+	return item
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
