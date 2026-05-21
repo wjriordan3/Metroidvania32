f192d65e3f5a24bc65490e30bad8d06970750bd2
riordan3 <70760257+wjriordan3@users.noreply.github.com>
2026-05-20 23:27:16 -0400

    WIP Enemy attack and hazard colliison

---
+++ Metroidvania32/damage_area.gd
@@ -0,0 +1,29 @@
+class_name DamageArea extends Area2D
+
+signal damage_taken( attack_area )
+
+@export var audio : AudioStream
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func take_damage( area : Area2D ) -> void:
+	if area is AttackArea:
+		damage_taken.emit( area )
+		print( "Attack damage taken", area.damage )
+	elif area is HazardArea:
+		damage_taken.emit( area )
+		print( "Hazard damage taken", area.damage )
+	pass
+	
+
+func make_invulnerable(duration : float = 1.0 ) -> void:
+	process_mode = Node.PROCESS_MODE_DISABLED
+	await get_tree().create_timer( duration ).timeout
+	process_mode = Node.PROCESS_MODE_INHERIT
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
