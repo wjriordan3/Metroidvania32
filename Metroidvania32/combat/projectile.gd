1e9045f10673bdfe0fc52b43157b04149eff0731
wjriordan3 <70760257+wjriordan3@users.noreply.github.com>
2026-05-26 19:52:14 -0400

    Add AttackArea to player projectiles

--- Metroidvania32/combat/projectile.gd
+++ Metroidvania32/combat/projectile.gd
@@ -2,9 +2,13 @@ extends Node2D
 
 const SPEED = 300
 
+func _ready() -> void :
+	$AttackArea.set_active( true )
+
 func _process(delta):
 	position += transform.x * SPEED * delta
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	$AttackArea.set_active( false )
 	queue_free()
