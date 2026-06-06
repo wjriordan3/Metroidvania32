e2c8263be0d9a7ed4aa1d22ba083645d5a0b3085
riordan3 <70760257+wjriordan3@users.noreply.github.com>
2026-06-05 22:03:28 -0400

    Enemy decision making framework

---
+++ Metroidvania32/enemy/scripts/decision_engine.gd
@@ -0,0 +1,14 @@
+class_name DecisionEngine extends Node
+
+var enemy : Enemy
+var current_state : EnemyState
+var blackboard : Blackboard
+
+func _ready() -> void :
+	while not enemy:
+		await get_tree().process_frame
+	enemy.change_dir( -1.0 if enemy.face_left_on_start else 1.0 )
+	pass
+	
+func decide() -> EnemyState :
+	return null
