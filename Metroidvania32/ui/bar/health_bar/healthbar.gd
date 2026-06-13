6147b2ed61ec385b84d7996c9139ba3f10aff30f
Brian <brianng2002@gmail.com>
2026-06-12 23:46:28 -0400

    Updated health, limb, and scrap UI

--- Metroidvania32/ui/bar/health_bar/healthbar.gd
+++ Metroidvania32/ui/bar/health_bar/healthbar.gd
@@ -2,16 +2,13 @@ extends HBoxContainer
 
 var max_health = 100.0
 @onready var hp_bar: TextureProgressBar = %HPBar
-@onready var label: Label = $Counter/Label
 
 func initialize(max):
 	max_health = max
 	hp_bar.max_value = max
-	label.text = "%s" % [max]
 	
 func update_health(health):
 	hp_bar.value = health
-	label.text = "%s" % [health]
 
 func _on_interface_health_changed(health):
 	update_health(health)
