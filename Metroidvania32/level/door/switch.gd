d8bd97ef154497b3a953defb77eb971ebb8b0347
Brian <brianng2002@gmail.com>
2026-06-14 11:30:59 -0400

    Door and switch bugfix

--- Metroidvania32/level/door/switch.gd
+++ Metroidvania32/level/door/switch.gd
@@ -31,20 +31,21 @@ func _ready() -> void:
 			switch_sprite.sprite_frames = button_switch
 
 func _activate():
-	active = true
-	switch_sprite.play()
-	Messages.switch_activated.emit(self)
+	if !active:
+		active = true
+		switch_sprite.play()
+		Messages.switch_activated.emit(self)
 
 # Button switch logic
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") && switch_type == SwitchType.BUTTON && !active:
+	if body.is_in_group("player") && switch_type == SwitchType.BUTTON:
 		print("Button pressed" + body.name)
 		_activate()
 
 # Gun and kick switch logic
 func _on_damage_area_damage_taken(attack_area: Variant) -> void:
 	print("Button attacked" + attack_area.name)
-	if switch_type == SwitchType.GUN && attack_area.collision_layer == 13:
+	if switch_type == SwitchType.GUN && attack_area.get_collision_layer_value(13):
 		_activate()
-	elif switch_type == SwitchType.KICK && attack_area.collision_layer == 11:
+	elif switch_type == SwitchType.KICK && attack_area.get_collision_layer_value(11):
 		_activate()
